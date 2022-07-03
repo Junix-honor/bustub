2e584922bfdd919b54e41ac7de05a328acfd3aee
Huang Junxun <957824346@qq,com>
2022-07-03 16:45:36 +0800

    LRU REPLACEMENT POLICY

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -18,12 +18,29 @@ LRUReplacer::LRUReplacer(size_t num_pages) {}
 
 LRUReplacer::~LRUReplacer() = default;
 
-auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool { return false; }
+auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool {
+  if (frame_list.empty()) return false;
+  *frame_id = frame_list.back();
+  frame_map.erase(*frame_id);
+  frame_list.pop_back();
+  return true;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+  if (frame_map.count(frame_id)) {
+    frame_list.erase(frame_map[frame_id]);
+    frame_map.erase(frame_id);
+  }
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+  if (frame_map.count(frame_id)) return;
+  frame_list.push_front(frame_id);
+  frame_map[frame_id] = frame_list.begin();
+}
 
-auto LRUReplacer::Size() -> size_t { return 0; }
+auto LRUReplacer::Size() -> size_t {
+  return frame_list.size();
+}
 
 }  // namespace bustub
--- src/include/buffer/lru_replacer.h
+++ src/include/buffer/lru_replacer.h
@@ -15,6 +15,7 @@
 #include <list>
 #include <mutex>  // NOLINT
 #include <vector>
+#include <map>
 
 #include "buffer/replacer.h"
 #include "common/config.h"
@@ -47,6 +48,8 @@ class LRUReplacer : public Replacer {
 
  private:
   // TODO(student): implement me!
+  std::list<frame_id_t> frame_list;
+  std::map<frame_id_t, std::list<frame_id_t>::iterator> frame_map;
 };
 
 }  // namespace bustub
